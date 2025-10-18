85495adb89a40686daf86d53aa9236cfa772c8d3
Mati <maf.vergara@duocuc.cl>
2025-10-17 22:08:52 -0300

    Primer commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function Navbar() {
+function NavbarPrincipal() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -31,4 +31,4 @@ function Navbar() {
   );
 }
 
-export default Navbar;
+export default NavbarPrincipal;
